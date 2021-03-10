b5b8b5fd2a5609b8778a8cd598f1a12e3258a251
Green1993 <Green1993.93@mail.ru>
2021-03-10 14:09:47 +0300

    Delete

--- HomeWork1.java
+++
@@ -1,7 +0,0 @@
-package com.grigoriy.idea;
-
-public class HomeWork1 {
-    public static void main (String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- HomeWork2.java
+++
@@ -1,7 +0,0 @@
-package com.grigoriy.idea;
-
-public class HomeWork2 {
-    public static void main(String[] args) {
-
-    }
-}
